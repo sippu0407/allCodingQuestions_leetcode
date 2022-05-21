6d0d642d5320a0895a76a6dad36660fbd7a49ef1
sippu0407 <suryabhan.singh.0407@gmail.com>
2022-05-21 12:05:04 +0530

    stack 1

--- stack/DupicateBracket_1.java
+++ stack/DupicateBracket_1.java
@@ -1,10 +1,38 @@
 package stack;
 
+import java.util.*;
+
 public class DupicateBracket_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		String exp=scn.nextLine();
+		
+		duplicateBracket(exp);
 	}
-
+    
+	public static void duplicateBracket(String exp) {
+		Stack<Character> st=new Stack<>();
+		
+		//logic
+		
+	for(int i=0;i<exp.length();i++) {
+		
+		char ch=exp.charAt(i);
+		
+		if(ch!=')') st.push(ch);
+		
+		else {
+			if(st.peek()=='(') {
+				System.out.print(true);
+				return;
+			}
+			
+			while(st.peek()!='(') st.pop();
+			st.pop();
+		}
+	}
+	System.out.print(false);
+		
+	}
 }
